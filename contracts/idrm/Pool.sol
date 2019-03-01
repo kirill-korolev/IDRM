867fd0b2e33838434d4d454ff87ecdd20b718aae
kirill-korolev <kafafyf@gmail.com>
2019-03-01 14:04:53 +0300

    Bug fix and functionality expansion

--- contracts/idrm/Pool.sol
+++ contracts/idrm/Pool.sol
@@ -1,16 +1,24 @@
 pragma solidity ^0.5.0;
 
-import "../math/SafeMath.sol";
-
 contract Pool {
-	using SafeMath for uint256;
 
+	// Basically master is the creator of a pool
+	// It can only be one person or any other number
+	// of masters up to 5 (exclusively)
+	//
+	// Currently all masters have similar rights and
+	// power and unlike arbitrators they're not elected
+	//
+	// Once a person wants to become an arbitrator
+	// one is added to a pending list and after that
+	// all of the masters must confirm that
 	struct Master {
 		mapping(address => bool) confirmedArbitrators;
 	}
 
 	struct Arbitrator {
 		address ethAddress;
+		uint256 reputation;
 	}
 
 	mapping(uint256 => Master) public masters;
@@ -23,6 +31,8 @@ contract Pool {
 	mapping(address => uint256) public arbitratorsIds;
 	uint256 public arbitratorsCount;
 
+	string public name;
+
 	event PendingArbitrator(address _arbitrator);
 
 	modifier onlyMasters {
@@ -30,26 +40,37 @@ contract Pool {
 		_;
 	}
 
-	modifier isPendingArbitrator {
+	modifier isPendingArbitrator(address _arbitrator) {
 		require(arbitratorsIds[_arbitrator] == 0);
 		_;
 	}
 
-	constructor(address[] _masters) public {
+	constructor(string _name, address[] _masters) public {
 		require(_masters.length > 0 && _masters.length < 5);
+
+		name = _name;
 
 		for(uint256 i = 0; i < _masters.length; ++i) {
 			mastersIds[_masters[i]] = i + 1;
+			masters[mastersIds[_masters[i]]] = Master();
 		}
 
 		mastersCount = _masters.length;
 	}
 
-	function becomeArbitrator() public isPendingArbitrator {
+	function becomeArbitrator() public isPendingArbitrator(msg.sender) {
+		require(mastersIds[msg.sender] == 0);
 		emit PendingArbitrator(msg.sender);
 	}
 
-	function confirmArbitrator(address _arbitrator) public onlyMasters isPendingArbitrator {
+	function leavePool() public {
+		uint256 id = arbitratorsIds[msg.sender];
+		require(id != 0);
+		delete arbitratorsIds[msg.sender];
+		delete arbitrators[id];
+	}
+
+	function confirmArbitrator(address _arbitrator) public onlyMasters isPendingArbitrator(_arbitrator) {
 		require(!masters[mastersIds[msg.sender]].confirmedArbitrators[_arbitrator]);
 		masters[mastersIds[msg.sender]].confirmedArbitrators[_arbitrator] = true;
 		pendingArbitrators[_arbitrator]++;
@@ -59,7 +80,7 @@ contract Pool {
 		}
 	}
 
-	function denyArbitrator(address _arbitrator) public onlyMasters isPendingArbitrator {
+	function denyArbitrator(address _arbitrator) public onlyMasters isPendingArbitrator(_arbitrator) {
 
 		for(uint256 i = 1; i < mastersCount; ++i) {
 			masters[i].confirmedArbitrators[_arbitrator] = false;
@@ -68,7 +89,7 @@ contract Pool {
 		pendingArbitrators[_arbitrator] = 0;
 	}
 
-	function addArbitrator(address _arbitrator) private onlyMasters isPendingArbitrator {
+	function addArbitrator(address _arbitrator) private onlyMasters isPendingArbitrator(_arbitrator) {
 		arbitratorsIds[_arbitrator] = ++arbitratorsCount;
 		arbitrators[arbitratorsIds[_arbitrator]] = Arbitrator(_arbitrator);
 	}
