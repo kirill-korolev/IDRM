68d893bdbd193fbcf760a7a80ef281c8991b0dc3
kirill-korolev <kafafyf@gmail.com>
2019-03-02 13:11:38 +0300

    Scripts update

--- src/js/account.js
+++ src/js/account.js
@@ -1,2 +1,36 @@
 jQuery(document).ready(function( $ ) {
+
+  var render = App.render;
+
+  App.render = function(){
+
+    render();
+
+    $('#create-pool').click(function(e) {
+      App.contracts.PoolFactory.deployed().then(function(instance){
+        var name = $('#pool-factory #name').val();
+        var addresses = [App.account];
+
+        $('#pool-factory input[name="address"]').each(function(i, elem){
+          addresses.push($(this).val());
+        });
+
+        instance.createPool(name, addresses);
+      });
+    });
+
+    App.contracts.PoolFactory.deployed().then(function(instance){
+      instance.poolsCount().then(function(poolsCount){
+        for(var i = 1; i <= poolsCount; ++i){
+          instance.pools(i).then(function(poolAddress){
+            var pool = App.contracts.Pool.at(poolAddress);
+            pool.name().then(function(name){
+              $('#pool-select').append('<option>' + name + '</option>');
+            });
+          });
+        }
+      });
+    });
+
+  }
 });
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function( $ ) {
+
   $('#add-address').click(function(e) {
 
       var count = $('.address-for-pool').length + 1;
@@ -15,4 +16,44 @@ jQuery(document).ready(function( $ ) {
       `;
       $('#add-address').before(input);
   });
+
+  var render = App.render;
+
+  App.render = function() {
+    render();
+    
+    App.contracts.PoolFactory.deployed().then(function(instance){
+      poolFactory = instance;
+    }).then(function(){
+      return poolFactory.poolsCount();
+    }).then(function(count){
+      for(var i = 1; i <= count; ++i){
+        poolFactory.pools(i).then(function(poolAddress){
+          var pool = App.contracts.Pool.at(poolAddress);
+
+          pool.name().then(function(name){
+            pool.arbitratorsCount().then(function(arbitratorsCount){
+              var block = `
+              <div class="col-lg-4 col-md-6">
+                <div class="speaker">
+                  <img src="https://miro.medium.com/max/1104/1*6bOXOdSXtre9t7aMgTr-4A.png" alt="Speaker 1" class="img-fluid">
+                  <div class="details">
+                    <h3><a href="pool.html?address=${poolAddress}">${name}</a></h3>
+                    <p>${arbitratorsCount} arbitrators</p>
+                  </div>
+                </div>
+              </div>
+              `;
+
+              $('#speakers .row').append(block);
+            });
+          });
+
+        });
+      }
+    });
+  }
+
+
+
 });
